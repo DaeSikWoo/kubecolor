f4a4f7f58b67b61ffea3bff3ac3772ecacb014fc
Danny Woo <35883117+DaeSikWoo@users.noreply.github.com>
2023-04-20 11:53:44 +0900

    Add files via upload

---
+++ kubecolor.sh
@@ -0,0 +1,9 @@
+wget https://github.com/hidetatz/kubecolor/releases/download/v0.0.25/kubecolor_0.0.25_Linux_x86_64.tar.gz &&
+
+tar -xzvf kubecolor_0.0.25_Linux_x86_64.tar.gz &&
+
+mv kubecolor /usr/local/bin/ &&
+
+echo "alias kubectl='kubecolor'" >> ~/.bashrc &&
+
+source ~/.bashrc
